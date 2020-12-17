30c436cb0066f62a6d4d17e4a173e0737ddf1d9e
Artemivo <body.serviceauto@gmail.com>
2020-12-18 00:21:33 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import ProductList from './home-work-1/ProductList.js';
+
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className='app'>
+        <ProductList />
+      </div>
+    )
+  }
+}
+
+export default App;
