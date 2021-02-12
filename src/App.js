f0b7d3a2bb85869a29c39dd17ea254506e9abf34
Artemivo <body.serviceauto@gmail.com>
2021-02-13 01:57:24 +0200

    home-work-8

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import {StatusUser} from './home-work-4/StatusUser.js';
 import {ChangeTitle} from './home-work-4/ChangeTitle.js';
 import BlogRout from './home-work-5/BlogRout.js'
 import MyShop from './home-work-6/MyShop'
+import TodoApp from './todo-list/TodoApp';
+import MyApp from './home-work-7/MyApp'
 
 
 
@@ -18,7 +20,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <MyShop />
+        <MyApp />
       </div>
     )
   }
